fe8020327d7dbe28d9bfc8d09efe3a3ecc3e564e
Moiseis Cruz <moizzito@gmail.com>
2024-02-20 12:46:17 -0300

    I converted the file to App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import { QrCode } from './component/qr-card/qr-card';
+
+function App() {
+  return (
+    <div className="App">
+      <QrCode />
+    </div>
+  );
+}
+
+export default App;
