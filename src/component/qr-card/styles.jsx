aa266889ebcdeed00d572c187f9874571558712f
Moiseis Cruz <moizzito@gmail.com>
2024-03-12 18:28:14 -0300

    I placed the styled files.

---
+++ src/component/qr-card/styles.jsx
@@ -0,0 +1,32 @@
+import styled from 'styled-components'
+
+export const Card = styled.div`
+    background-color: #fff;
+    width: 315px;
+    height: 484px;
+    border-radius: 15px;
+    font-family: 'Outfit', sans-serif;
+`
+
+export const Image = styled.img`
+    width: 280px;
+    height: 280px;
+    margin-top: 18px;
+    border-radius: 15px;
+`
+
+export const Title = styled.h2`
+    width: 254px;
+    margin: 9px auto 0;
+    font-size: 22px;
+    font-weight: 600;
+    color: #1F3251;
+`
+
+export const Description = styled.p`
+    width: 243px;
+    font-size: 15px;
+    margin: 19px auto 0;
+    font-weight: 400;
+    color: #7B879D;
+`
