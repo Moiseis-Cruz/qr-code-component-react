9690ce13a3e7e318103d7a1f4aaf7aef2a993a91
Moiseis Cruz <moizzito@gmail.com>
2024-03-12 18:27:32 -0300

    I removed the styles from the component file.

--- src/component/qr-card/qr-card.jsx
+++ src/component/qr-card/qr-card.jsx
@@ -1,5 +1,6 @@
 import ImgQrCode from '../img/image-qr-code.png'
-import styled from 'styled-components'
+
+import { Card, Image, Title, Description } from './styles'
 
 const QrCode = function () {
     return(
@@ -12,36 +13,5 @@ const QrCode = function () {
         </Card>
     )
 }
-
-const Card = styled.div`
-    background-color: #fff;
-    width: 315px;
-    height: 484px;
-    border-radius: 15px;
-    font-family: 'Outfit', sans-serif;
-`
-
-const Image = styled.img`
-    width: 280px;
-    height: 280px;
-    margin-top: 18px;
-    border-radius: 15px;
-`
-
-const Title = styled.h2`
-    width: 254px;
-    margin: 9px auto 0;
-    font-size: 22px;
-    font-weight: 600;
-    color: #1F3251;
-`
-
-const Description = styled.p`
-    width: 243px;
-    font-size: 15px;
-    margin: 19px auto 0;
-    font-weight: 400;
-    color: #7B879D;
-`
 
 export { QrCode }
